dde2a39b2544f26264205228d66fdbb9144f5113
Ayush Gupta <46589053+ayushgupta9198@users.noreply.github.com>
2020-04-07 14:39:27 +0530

    change device to xla_gpu

--- training/training_loop.py
+++ training/training_loop.py
@@ -146,7 +146,7 @@ def training_loop(
     training_set = dataset.load_dataset(data_dir=config.data_dir, verbose=True, **dataset_args)
 
     # Construct networks.
-    with tf.device('/gpu:0'):
+    with tf.device('/device:XLA_GPU:0'):
         if resume_run_id is not None:
             network_pkl = misc.locate_network_pkl(resume_run_id, resume_snapshot)
             print('Loading networks from "%s"...' % network_pkl)
@@ -169,7 +169,7 @@ def training_loop(
     G_opt = tflib.Optimizer(name='TrainG', learning_rate=lrate_in, **G_opt_args)
     D_opt = tflib.Optimizer(name='TrainD', learning_rate=lrate_in, **D_opt_args)
     for gpu in range(submit_config.num_gpus):
-        with tf.name_scope('GPU%d' % gpu), tf.device('/gpu:%d' % gpu):
+        with tf.name_scope('GPU%d' % gpu), tf.device('/device:XLA_GPU:%d' % gpu):
             G_gpu = G if gpu == 0 else G.clone(G.name + '_shadow')
             D_gpu = D if gpu == 0 else D.clone(D.name + '_shadow')
             lod_assign_ops = [tf.assign(G_gpu.find_var('lod'), lod_in), tf.assign(D_gpu.find_var('lod'), lod_in)]
@@ -185,7 +185,7 @@ def training_loop(
     D_train_op = D_opt.apply_updates()
 
     Gs_update_op = Gs.setup_as_moving_average_of(G, beta=Gs_beta)
-    with tf.device('/gpu:0'):
+    with tf.device('/device:XLA_GPU:0'):
         try:
             peak_gpu_mem_op = tf.contrib.memory_stats.MaxBytesInUse()
         except tf.errors.NotFoundError:
